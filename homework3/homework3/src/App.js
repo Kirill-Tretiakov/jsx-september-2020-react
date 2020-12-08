64c42fa0156881742163070c1b88187562dad0bd
HOME <kirill.tretyakoff@gmail.com>
2020-12-08 15:20:05 +0200

    My 3 React homework

---
+++ homework3/homework3/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import AllUserComponent from "./components/Users/AllUserComponent";
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <AllUserComponent/>
+            </div>
+        );
+    }
+}
+
+export default App;
