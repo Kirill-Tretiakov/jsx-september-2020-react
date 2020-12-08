7b72ab1d5a9ed754b89045a98da154ab9cf51a46
HOME <kirill.tretyakoff@gmail.com>
2020-12-08 15:42:39 +0200

    My 3 React homework

---
+++ homework3/src/services/UserServices/UserService.js
@@ -0,0 +1,12 @@
+export class UserService {
+
+    url = 'https://jsonplaceholder.typicode.com/users';
+
+    getAllUsers() {
+        return (
+            fetch(this.url)
+                .then(value => value.json())
+                .then(value => value)
+        )
+    }
+}
