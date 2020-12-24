8a7a194b59940fa1634602f395e060101a8128e9
HOME <kirill.tretyakoff@gmail.com>
2020-12-24 09:59:12 +0200

    My 6 React homework

---
+++ homework6/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import FormComponent from "./components/FormComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <FormComponent/>
+            </div>
+        );
+    }
+}
+
+export default App;
